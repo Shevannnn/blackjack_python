8407f58e7f7d3fd197d23086c8d844dc076af19a
Evan <evaneldridge.palo@gmail.com>
2025-03-27 22:05:25 +0800

    simplified deck.py, renamed vars in card.py

--- prelim_introtoml/deck.py
+++ prelim_introtoml/deck.py
@@ -5,11 +5,16 @@ class Deck:
     def __init__(self):
         suits = ['D', 'H', 'S', 'C']
         faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        self.cards = [Card(suit, face) for suit in suits for face in faces]
-        self.shuffle_deck()
+        self.cards = []
 
+        for suit in suits:
+            for face in faces:
+                self.cards.append(Card(suit, face))
+
+        self.shuffle_deck()
+        
     def shuffle_deck(self):
         random.shuffle(self.cards)
 
     def draw_card(self):
-        return self.cards.pop() if self.cards else None
+        return self.cards.pop()
